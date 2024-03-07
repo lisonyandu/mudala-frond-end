b63d8e051ac4f36a5ab22c4f99ab124f3c963315
Liso Nyandu <51886336+lisonyandu@users.noreply.github.com>
2024-03-07 13:30:50 +0200

    update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -3,14 +3,5 @@ const path = require("path");
 
 module.exports = defineConfig({
   transpileDependencies: true,
-  outputDir: path.resolve(__dirname, "../public"),
-  configureWebpack: {
-    resolve: {
-      fallback: {
-        "path": require.resolve("path-browserify"),
-        "os": require.resolve("os-browserify/browser"),
-        "fs": require.resolve("fs-browserify"),
-      }
-    }
-  }
-});
+  outputDir: path.resolve(__dirname, "../public")
+})
